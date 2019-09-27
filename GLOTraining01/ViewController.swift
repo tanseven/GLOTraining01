f6b2cf9f63b0ce992b7ca6d35776b83681aa7ab7
Tanapong Borrirakwisitsak <tanapong.b@gmail.com>
2019-09-27 15:17:02 +0700

    Initial.

---
+++ GLOTraining01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GLOTraining01
+//
+//  Created by Tanapong Borrirakwisitsak on 27/9/2562 BE.
+//  Copyright © 2562 ClickNext. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
